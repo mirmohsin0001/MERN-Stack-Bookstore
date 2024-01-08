754dce9213f23ff449c920356551444be5ec1c7d
mirmohsin0001 <mirmohsin0001@gmail.com>
2024-01-08 21:34:21 +0530

    Updated

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://<username>:<password>@book-store-mern.mfuqgka.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://root:root@book-store-mern.mfuqgka.mongodb.net/?retryWrites=true&w=majority";
